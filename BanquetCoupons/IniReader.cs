a1864892802a9014f4ea563fb3516f35e5ecd603
DEV <64015572@rbac.ac.th>
2025-05-17 17:46:02 +0700

    Add project files.

---
+++ BanquetCoupons/IniReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class IniReader
+{
+    public static Dictionary<string, string> ReadIni(string filepath, string section)
+    {
+        var config = new Dictionary<string, string>();
+        string[] lines = File.ReadAllLines(filepath);
+        bool inSection = false;
+
+        foreach (string line in lines)
+        {
+            string trimmed = line.Trim();
+            
+            if (string.IsNullOrWhiteSpace(trimmed) || trimmed.StartsWith(";") || trimmed.StartsWith("#")) //ข้ามบรรทัดว่าง
+            continue; 
+
+            if (trimmed.StartsWith("[") && trimmed.EndsWith("]"))
+            {
+                inSection = trimmed.Substring(1, trimmed.Length - 2).Equals(section, StringComparison.OrdinalIgnoreCase);
+                continue;
+            }
+
+            if (inSection && trimmed.Contains("="))
+            {
+                var parts = trimmed.Split('=', (char)2);
+                if (parts.Length == 2)
+                    config[parts[0].Trim()] = parts[1].Trim();
+            }
+        }
+        return config;
+    }
+}
---
+++ BanquetCoupons/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "config.ini";
+        var config = IniReader.ReadIni(path, "Database");
+
+        Console.WriteLine("ค่าจากไฟล์ INI : ");
+        foreach(var item in config)
+        {
+            Console.WriteLine($"{item.Key} = {item.Value}");
+        }
+        //MessageBox.Show("Connect success");
+
+        string connectionString = $"Server={config["Server"]};Database={config["Database"]};User Id={config["User"]};Password={config["Password"]};";
+        Console.WriteLine("\n Connection String: ");
+        Console.WriteLine(connectionString);
+    }
+}
